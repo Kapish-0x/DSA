74db50cc16e890f6e9121a19ffa9d69f85d21878
Kapish-0x <kapishsinghrawat@gmail.com>
2026-07-17 22:54:21 +0530

    Time: 2 ms (94.02%), Space: 64.7 MB (89.49%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,36 +3,23 @@ class Solution {
         int n = nums.length;
         int[] pre = new int[n];
         int[] suf = new int[n];
-        int[] res = new int[n];
-
-        // prefix product
-        pre[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i-1] * nums[i];
+        int[] ans = new int[n];
+        pre[0] = 1;
+        suf[n-1] = 1;
+        //Prefix Prod
+        for(int i = 1; i < n; ++i) {
+            pre[i] = pre[i-1] * nums[i-1];
+        }
+        //Suffix Prod
+        for(int i = n - 2; i >= 0; --i) {
+            suf[i] = suf[i+1] * nums[i+1];
         }
 
-        // suffix product
-        suf[n-1] = nums[n-1];
-        for(int i = n-2; i >= 0; i--) {
-            suf[i] = suf[i+1] * nums[i];
+        //answer
+        for(int i = 0; i < n; ++i) {
+            ans[i] = pre[i] * suf[i];
         }
 
-        // result
-        for (int i = 0; i < n; i++) {
-            int left, right;
-            if (i == 0) {
-                left = 1;
-            } else {
-                left = pre[i - 1];
-            }
-            if (i == n - 1) {
-                right = 1;
-            } else {
-                right = suf[i + 1];
-            }
-            res[i] = left * right;
-        }
-
-        return res;
+        return ans;
     }
 }
