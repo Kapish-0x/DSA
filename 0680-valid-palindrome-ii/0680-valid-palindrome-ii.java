9f8b2949e51edd6c2b79f66b10bf2eeeb07e279d
Kapish-0x <kapishsinghrawat@gmail.com>
2026-07-17 23:22:25 +0530

    Time: 4 ms (99.67%), Space: 47 MB (99.49%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                // try skipping either the left char or the right char
+                return isPalindromeRange(s, i + 1, j) || isPalindromeRange(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+    private boolean isPalindromeRange(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
