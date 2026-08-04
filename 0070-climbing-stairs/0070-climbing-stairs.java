dd4095ff9fe198b81f2ca98ac71d43a50bd38d6e
Kapish-0x <kapishsinghrawat@gmail.com>
2026-08-04 20:25:18 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,31 @@
+// (Memoization + Recursion)
+// class Solution {
+//     int[] t = new int[46];
+//     public int solve(int n) {
+//         if(n < 0) return 0;
+//         if(n == 0) return 1;
+//         if(t[n] != -1) return t[n];
+//         return t[n] = solve(n-1) + solve(n-2);
+//     }
+//     public int climbStairs(int n) {
+//         Arrays.fill(t, -1);
+//         return solve(n);
+//     }
+// }
+
+//Bottom-up
 class Solution {
-    int[] t = new int[46];
-    public int solve(int n) {
-        if(n < 0) return 0;
-        if(n == 0) return 1;
-        if(t[n] != -1) return t[n];
-        return t[n] = solve(n-1) + solve(n-2);
-    }
     public int climbStairs(int n) {
-        Arrays.fill(t, -1);
-        return solve(n);
+        if(n == 0 || n == 1 || n == 2) {
+            return n;
+        }
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i = 3; i <= n; ++i) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
     }
 }
