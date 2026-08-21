66beb48bbdb2f84ad289e65e73e6d27c4f8afd4c
Kapish-0x <kapishsinghrawat@gmail.com>
2026-08-21 20:40:27 +0530

    Time: 1 ms (99.66%), Space: 42.6 MB (93.31%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,12 +1,33 @@
+//Brute Force
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+//         int n = stones.length;
+//         for(int i = 0; i < n - 1; ++i) {
+//             Arrays.sort(stones);
+//             stones[n-1] = stones[n-1] - stones[n-2];
+//             stones[n-2] = 0;
+//         }
+//         Arrays.sort(stones);
+//         return stones[n-1];
+//     }
+// }
+
+//Priority Queue (MAX HEAP)
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        int n = stones.length;
-        for(int i = 0; i < n - 1; ++i) {
-            Arrays.sort(stones);
-            stones[n-1] = stones[n-1] - stones[n-2];
-            stones[n-2] = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); //reverse because in java pq is default a min heap and here we need max heap
+        for(int i : stones) {
+            pq.add(i);
         }
-        Arrays.sort(stones);
-        return stones[n-1];
+        while(pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+
+            if(first != second) {
+                pq.add(first - second);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.peek();
     }
 }
+
