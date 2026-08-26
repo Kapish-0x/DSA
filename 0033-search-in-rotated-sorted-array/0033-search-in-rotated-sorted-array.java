0535141208f3663eb2cf558dbe711f64c4871b97
Kapish-0x <kapishsinghrawat@gmail.com>
2026-08-26 13:33:34 +0530

    Time: 0 ms (100%), Space: 43.9 MB (28.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,7 +1,35 @@
+//O(n)
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         for(int i = 0; i < nums.length; ++i) {
+//             if(nums[i] == target) return i;
+//         }
+//         return -1;
+//     }
+// }
+
+//Binary Search
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i = 0; i < nums.length; ++i) {
-            if(nums[i] == target) return i;
+        int n = nums.length;
+        int left = 0;
+        int right = n-1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[left] <= nums[mid]) {
+                if(nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if(nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = right - 1;
+                }
+            }
         }
         return -1;
     }
