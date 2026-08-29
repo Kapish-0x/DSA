d03094eff1e08cf736bdab7940c081a07a82e04f
Kapish-0x <kapishsinghrawat@gmail.com>
2026-08-29 10:40:24 +0530

    Time: 3 ms (86.07%), Space: 43.1 MB (85.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
+        Stack <Character> st = new Stack<>();
         for(int i = 0; i < s.length(); ++i) {
             char c = s.charAt(i);
-            if(c == '(' || c == '[' || c == '{') {
+            if(c == '{' || c == '[' || c == '(') {
                 st.push(c);
             } else {
                 if(st.isEmpty()) {
-                    return false;   //if its a closing bracket but the stack is empty return false as its invalid
+                    return false;
+                } else {
+                    char top = st.pop();
+                    if(c == ']' && top != '[') return false;
+                    if(c == ')' && top != '(') return false;
+                    if(c == '}' && top != '{') return false;
                 }
-                char top = st.pop();
-                if(c == ')' && top != '(') return false;
-                if(c == ']' && top != '[') return false;
-                if(c == '}' && top != '{') return false;
             }
         }
         return st.isEmpty();
