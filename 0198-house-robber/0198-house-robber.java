72dd225734a157f85d85ce821a203392149f099a
Kapish-0x <kapishsinghrawat@gmail.com>
2026-09-02 22:25:29 +0530

    Time: 0 ms (100%), Space: 42.4 MB (97.3%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,16 @@
 //Memoization
 class Solution {
-    int memo[] = new int[101];
+    int[] memo = new int[101];
+    public int rob(int[] nums) {
+        Arrays.fill(memo, -1);
+        return solve(nums, 0);
+    }
     public int solve(int[] nums, int i) {
         if(i >= nums.length) return 0;
         if(memo[i] != -1) return memo[i];
         int steal = nums[i] + solve(nums, i+2);
         int skip = solve(nums, i+1);
         return memo[i] = Math.max(steal, skip);
-    }
-    public int rob(int[] nums) {
-        Arrays.fill(memo, -1);
-        return solve(nums, 0);
     }
 }
 
