651bd840315acd9786536c6e9e734af047547c25
Kapish-0x <kapishsinghrawat@gmail.com>
2026-09-02 20:45:16 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.55%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,36 @@
-//recursion+memoization (top-down)
-class Solution { 
-    int[] memo = new int[31];
+//recursion
+// class Solution {
+//     public int fib(int n) {
+//         if( n <= 1) return n;
+//         int res = fib(n-1)+fib(n-2);
+//         return res;
+//     }
+// }
 
-    public Solution() {
-        java.util.Arrays.fill(memo, -1);
-    }
+//recursion+memoization (top-down)
+// class Solution { 
+//     int[] memo = new int[31];
+//     public Solution() {
+//         Arrays.fill(memo, -1);
+//     }
+//     public int fib(int n) {
+//         if(n <= 1) return n;
+//         if(memo[n] != -1) return memo[n];
+//         return memo[n] = fib(n-1) + fib(n-2);
+//     }
+// }
+
+//DP (bottom-up)
+class Solution {
     public int fib(int n) {
-        if(n <= 1) return n;
-        if(memo[n] != -1) return memo[n];
-        return memo[n] = fib(n-1) + fib(n-2);
+        int[] dp = new int[31];
+        if( n <= 1) return n;
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; ++i) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
