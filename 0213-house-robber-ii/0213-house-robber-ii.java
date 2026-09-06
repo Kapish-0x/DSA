8ce8cc51c574ebe36ca9794b6a11da25b14ab4b6
Kapish-0x <kapishsinghrawat@gmail.com>
2026-09-06 12:03:07 +0530

    Time: 0 ms (100%), Space: 42.8 MB (41.51%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -20,25 +20,56 @@
 // }
 
 
+//bottom-up constant space O(1)
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         if (n == 1) return nums[0];
 
+//         return Math.max(robRange(nums, 0, n - 2), robRange(nums, 1, n - 1));
+//     }
+
+//     private int robRange(int[] nums, int start, int end) {
+//         int prev2 = 0;
+//         int prev1 = 0;
+
+//         for (int i = start; i <= end; i++) {
+//             int current = Math.max(prev1, prev2 + nums[i]);
+//             prev2 = prev1;
+//             prev1 = current;
+//         }
+
+//         return prev1;
+//     }
+// }
+
+//Bottom-up
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
-
-        return Math.max(robRange(nums, 0, n - 2), robRange(nums, 1, n - 1));
-    }
-
-    private int robRange(int[] nums, int start, int end) {
-        int prev2 = 0;
-        int prev1 = 0;
-
-        for (int i = start; i <= end; i++) {
-            int current = Math.max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = current;
+        if(n == 1) {
+            return nums[0];
         }
-
-        return prev1;
+        int[] t = new int[n + 1];
+        t[0] = 0;
+        t[1] = nums[0];
+        //case1 taking the first house hence skipping the last one
+        for(int i = 1; i <= n-1; ++i) {
+            int skip = t[i-1];
+            int steal = nums[i-1] + ((i-2 >= 0) ? t[i-2] : 0);
+            t[i] = Math.max(skip, steal);
+        }
+        int result1 = t[n-1];
+        Arrays.fill(t, 0);
+        t[0] = 0;
+        t[1] = 0;
+        //case2 skipping the 1st house hence can take the last house 
+        for(int i = 2; i <= nums.length; ++i) {
+            int skip = t[i-1];
+            int steal = nums[i-1] + ((i-2 >= 0) ? t[i-2] : 0);
+            t[i] = Math.max(skip, steal);
+        }
+        int result2 = t[n];
+        return Math.max(result1, result2);
     }
 }
