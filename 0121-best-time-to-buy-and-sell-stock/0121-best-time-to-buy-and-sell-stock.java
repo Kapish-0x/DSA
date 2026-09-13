a18a51d27cead1a217e5965522a2302fe9a63fe3
Kapish-0x <kapishsinghrawat@gmail.com>
2026-09-13 11:53:54 +0530

    Time: 1 ms (99.95%), Space: 94.4 MB (51.5%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,3 +1,23 @@
+//Brute Force (O(n^2))
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int maxPro = 0;
+//         for(int i = 0; i < n; i++) {
+//             for(int j = i + 1; j < n; j++) {
+//                 int currProfit = prices[j] - prices[i];
+//                 if(currProfit > maxPro) {
+//                     maxPro = currProfit;
+//                 }
+//             }
+//         }
+//         return maxPro;
+//     }
+// }
+
+
+
+//Optimized 
 class Solution {
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
